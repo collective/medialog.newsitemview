7d6ebace2a40063364c7b4f11d7a1787c8ef279a
Espen Moe-Nilssen <espen@medialog.no>
2011-11-15 17:15:48 +0100

    removed message factory from vocabulary

--- medialog/newsitemview/vocabulary.py
+++ medialog/newsitemview/vocabulary.py
@@ -1,9 +1,7 @@
-from collective.easyslider import easyslider_message_factory as _
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from Products.CMFCore.utils import getToolByName
 from zope.component import getUtility 
 from zope.schema.interfaces import IVocabularyFactory 
-from Acquisition import aq_inner
 from zope.app.component.hooks import getSite
 
 
