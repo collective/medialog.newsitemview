0ed01f210927f5dbccd8fd7b485e38ee7e6de7cd
Espen Moe-Nilssen <espen@medialog.no>
2012-05-24 09:35:55 +0200

    Version 0.3.8 using browser view and fixes unicode error in caption

--- medialog/newsitemview/vocabulary.py
+++ medialog/newsitemview/vocabulary.py
@@ -7,10 +7,8 @@ from zope.app.component.hooks import getSite
 
 
 def ImageSizeVocabulary():
-    context = getSite()
-    #Cant get this to work, so stick with 3 sizes for now
-    #portal_properties = getToolByName(context, 'portal_properties', None)
-    #sizes = portal_properties.imaging_properties.getProperty('allowed_sizes')  
+    #Cant get this to work, so I stick with 3 sizes for now
+    #portal_properties = getToolByName(object, 'portal_properties', None)
     #sizes = portal_properties.imaging_properties.getProperty('allowed_sizes')  
     #terms = [title=pair for pair in sizes ]
     return ['thumb', 'mini', 'preview', 'large', 'none']  
