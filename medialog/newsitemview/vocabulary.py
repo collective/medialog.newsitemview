7d191df20e0d0eaf81e9a41c5d79e74137981d53
Espen Moe-Nilssen <espen@medialog.no>
2013-03-21 14:42:26 +0100

    commenting out the vocabulary in vocabulary.py

--- medialog/newsitemview/vocabulary.py
+++ medialog/newsitemview/vocabulary.py
@@ -5,10 +5,10 @@
 from zope.app.component.hooks import getSite
 #from zope.interface import alsoProvides 
 
-from five import grok
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary
-from Products.CMFCore.utils import getToolByName
+#from five import grok
+#from zope.schema.interfaces import IContextSourceBinder
+#from zope.schema.vocabulary import SimpleVocabulary
+#from Products.CMFCore.utils import getToolByName
 
 
 def format_size(size):
@@ -16,19 +16,19 @@ def format_size(size):
  
 
 
-@grok.provider(IContextSourceBinder)
-def ImageSizeVocabulary(self, context):
-    site = self.getSite()
-    sizes = ['thumb', 'thumb', 'mini', 'preview', 'large', 'none'] 
-    portal_properties = getToolByName(site, 'portal_properties', None)
-    if 'imaging_properties' in portal_properties.objectIds():
-        sizes = portal_properties.imaging_properties.getProperty(
-        'allowed_sizes')
-    terms = [SimpleTerm(value=format_size(pair),
-            token=format_size(pair),
-            title=pair) for pair in sizes]
-            
-    return SimpleVocabulary(terms)
+#@grok.provider(IContextSourceBinder)
+#def ImageSizeVocabulary(self, context):
+#    site = self.getSite()
+#    sizes = ['thumb', 'thumb', 'mini', 'preview', 'large', 'none'] 
+#    portal_properties = getToolByName(site, 'portal_properties', None)
+#    if 'imaging_properties' in portal_properties.objectIds():
+#        sizes = portal_properties.imaging_properties.getProperty(
+#        'allowed_sizes')
+#    terms = [SimpleTerm(value=format_size(pair),
+#            token=format_size(pair),
+#            title=pair) for pair in sizes]
+#            
+#    return SimpleVocabulary(terms)
 
 
 
